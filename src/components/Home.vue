0d5d97a33650c73df0877a786577e01e1ac59a5e
DDelgadoD <david.delgado82@gmail.com>
2023-02-04 13:34:04 +0100

    Nivell 1 - Exercici 1

---
+++ src/components/Home.vue
@@ -0,0 +1,39 @@
+<template>
+    <div> 
+      ¿Qué vols fer?
+    </div>
+    <div>
+      <input type="checkbox" id="checkbox0" v-on:click = "totalPrice(0)" /> 
+      <label for="checkbox">Una pàgina web ({{ price[0] }}€)</label>
+    </div>
+    <div>
+      <input type="checkbox" id="checkbox1" v-on:click = "totalPrice(1)" />
+      <label for="checkbox">Una pàgina web ({{ price[1] }}€)</label>
+    </div>
+    <div>
+      <input type="checkbox" id="checkbox2" v-on:click = "totalPrice(2)" /> 
+      <label for="checkbox">Una pàgina web ({{ price[2] }}€)</label>
+    </div>
+  
+    <div>
+      Preu: {{ total}}
+    </div>
+  </template>
+  
+  <script setup>
+    import { ref } from "vue";
+  
+    const check = ref([false, false, false])
+    const price = ref([500, 300, 200])
+    const total = ref(0)
+  
+    const totalPrice = (i) => {
+      check.value[i] = !check.value[i]
+      check.value[i]  ? total.value += price.value[i] : total.value -= price.value[i] 
+    }
+  
+  </script>
+  
+  <style scoped>
+  </style>
+  
---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const routes = [
+  {
+    path: "/",
+    component: () => import(/* webpackChunkName: '' */ "../components/Home.vue"),
+  },
+  {
+    path: "/home",
+    component: () => import(/* webpackChunkName: 'home' */ "../components/Home.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes,
+});
+
+export default router;
